a2b5fbc4b8317267836c160eef9335b265bc25fb
Shibdrex <124347191+Shibdrex@users.noreply.github.com>
2023-02-03 12:28:55 +0100

    Update Schiffe_Versenken.c

--- Schiffe_Versenken.c
+++ Schiffe_Versenken.c
@@ -1,26 +1,30 @@
-#include <stdlib.h>
 #include <stdio.h>
 #define SIZE 10
 char Spieler1[50], Spieler2[50];
 int SpielfeldSpieler[SIZE][SIZE];
 int SpielfeldGegner[SIZE][SIZE];
+char koordinaten[2];
 
 void createGrid();
 void Beginning();
 void feldkoordinate();
+void treffer();
+void Schuss();
 
 int main(int argc, char **argv)
 {
-	for(int i = 0 ; i < SIZE;i++){
-	for(int y = 0 ;	y < SIZE;y++){
-		SpielfeldSpieler[i][y] = 0 ;
-		SpielfeldGegner[i][y] = 0 ;
-		}
-	}
-	
+    for(int i = 0 ; i < SIZE;i++){
+        for(int y = 0 ;	y < SIZE;y++){
+            SpielfeldSpieler[i][y] = 0 ;
+            SpielfeldGegner[i][y] = 0 ;
+        }
+    }
+    Beginning();
+    createGrid();
+    Schuss();
 
-	return 0;
-	return 0;
+    return 0;
+
 }
 
 void Beginning()
@@ -39,24 +43,39 @@ void feldkoordinate()
     scanf("%s", &koordinaten);
     koordinaten[0] = koordinaten[0] - 65;
     koordinaten[1] = koordinaten[1] - 48;
-    printf("Spalte %d, Zeile %d \n", koordinaten[0], koordinaten[1]);
 }
 
 void createGrid(){
-	for(int i = 0 ; i < SIZE;i++){
-		printf("-----------------------------------------\n");
-		for(int y = 0 ;	y < SIZE;y++){
-			switch(SpielfeldSpieler[i][y]) {
-				case 0: 	printf("|   "); break;
-				case 1:		printf("| O "); break;
-				case 2:		printf("| X "); break;
-				case 3:		printf("| ~ "); break;
-			   default: 	printf("|   "); break;
-				}
-			}
-		printf("|");
-		printf("\n");
-		}
-		printf("-----------------------------------------");
-	}
+    for(int i = 0 ; i < SIZE;i++){
+        printf("-----------------------------------------\n");
+        for(int y = 0 ;	y < SIZE;y++){
+            switch(SpielfeldSpieler[i][y]) {
+                case 0: 	printf("|   "); break;
+                case 1:		printf("| O "); break;
+                case 2:		printf("| X "); break;
+                case 3:		printf("| ~ "); break;
+                default: 	printf("|   "); break;
+            }
+        }
+        printf("|");
+        printf("\n");
+    }
+    printf("-----------------------------------------");
+}
 
+void Schuss()
+{
+    printf("\n %s schiesst \n", &Spieler1);
+    feldkoordinate();
+    treffer();
+}
+
+void treffer()
+{
+    switch(SpielfeldSpieler[koordinaten[0]][koordinaten[1]]){
+        case 0: SpielfeldSpieler[koordinaten[0]][koordinaten[1]] = 3; printf("Du hast nichts getroffen"); break;
+        case 1: SpielfeldSpieler[koordinaten[0]][koordinaten[1]] = 2; printf("Du hast getroffen!"); break;
+        case 2: printf("Das Boot ist bereits getroffen"); Schuss(); break;
+        case 3: printf("Hier hast du schonmal hingeschossen, hier ist nichts"); Schuss(); break;
+    }
+}
