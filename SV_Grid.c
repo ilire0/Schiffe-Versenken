e33cd181bff9d9924d0674536d85233ca3984e7d
DerAlbaner180 <72442005+DerAlbaner180@users.noreply.github.com>
2023-02-03 12:43:04 +0100

    Update SV_Grid.c

--- SV_Grid.c
+++ SV_Grid.c
@@ -21,33 +21,35 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-
 void createGrid(){
-		printf("           ");
+//ABC Koordinaten
+	printf("           ");
 	for(unsigned char a = 0 ; a < SIZE; a++){
-		unsigned char wert = 65;
-		wert = wert + a;
-		printf("| %c ",wert);
+			unsigned char wert = 65;
+			wert = wert + a;
+			printf("| %c ",wert);
 		}
-		printf("|\n");
-		printf("           -----------------------------------------\n");
-		printf("\n");
+	printf("|\n");
+	printf("           -----------------------------------------\n");
+	printf("\n");
 		
-		int x = 0  ;
+//123 Koordinaten & Feldanzeige
+	int x = 0  ;
 	for(int i = 0 ; i < SIZE;i++){
 		printf("           -----------------------------------------\n");
 		printf("| %d |      ",x);
-			x++;
-		for(int y = 0 ;	y < SIZE;y++){
-			switch(SpielfeldSpieler[i][y]) {
-				case 0: 	printf("|   "); break;
-				case 1:		printf("| O "); break;
-				case 2:		printf("| X "); break;
-				case 3:		printf("| ~ "); break;
-			   default: 	printf("|   "); break;
+		x++;
+			for(int y = 0 ;	y < SIZE;y++){
+				switch(SpielfeldSpieler[i][y]) {
+					case 0: 	printf("|   "); break;
+					case 1:		printf("| O "); break;
+					case 2:		printf("| X "); break;
+					case 3:		printf("| ~ "); break;
+					default: 	printf("|   "); break;
 				}
 			}
 		printf("|\n");
-		}
-		printf("           -----------------------------------------");
+	}
+	printf("           -----------------------------------------\n");
+	printf("\n");
 	}
