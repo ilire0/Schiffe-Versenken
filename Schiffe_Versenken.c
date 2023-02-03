870958d646259c0c1bdbc4efdc99cbf8edfba67d
Trxtxn718 <123907625+Trxtxn718@users.noreply.github.com>
2023-02-03 10:46:26 +0100

    Update Schiffe_Versenken.c

--- Schiffe_Versenken.c
+++ Schiffe_Versenken.c
@@ -1,9 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+char Spieler1[50], Spieler2[50];
 
+void Beginning();
 int main(int argc, char **argv)
 {
 	
 	return 0;
 }
 
+void Beginning()
+{
+    printf("Willkommen zu Schiffe versenken\nBitte Spielername eingeben:\nSpieler 1:");
+    scanf("%s", &Spieler1);
+    printf("Spieler2:");
+    scanf("%s", &Spieler2);
+    return;
+}
