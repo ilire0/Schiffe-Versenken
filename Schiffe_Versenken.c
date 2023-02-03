9ac714043e746d7f8542aa40ea94c603c105b22c
DerAlbaner180 <72442005+DerAlbaner180@users.noreply.github.com>
2023-02-03 11:15:34 +0100

    Create Schiffe_Versenken.c

--- Schiffe_Versenken.c
+++ Schiffe_Versenken.c
@@ -1,14 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define SIZE 10
 char Spieler1[50], Spieler2[50];
+int SpielfeldSpieler[SIZE][SIZE];
+int SpielfeldGegner[SIZE][SIZE];
 
+void createGrid();
 void Beginning();
 void feldkoordinate();
 
 int main(int argc, char **argv)
 {
- Beginning();
- feldkoordinate();
+	for(int i = 0 ; i < SIZE;i++){
+	for(int y = 0 ;	y < SIZE;y++){
+		SpielfeldSpieler[i][y] = 0 ;
+		SpielfeldGegner[i][y] = 0 ;
+		}
+	}
+	
+
+	return 0;
 	return 0;
 }
 
@@ -30,3 +41,22 @@ void feldkoordinate()
     koordinaten[1] = koordinaten[1] - 48;
     printf("Spalte %d, Zeile %d \n", koordinaten[0], koordinaten[1]);
 }
+
+void createGrid(){
+	for(int i = 0 ; i < SIZE;i++){
+		printf("-----------------------------------------\n");
+		for(int y = 0 ;	y < SIZE;y++){
+			switch(SpielfeldSpieler[i][y]) {
+				case 0: 	printf("|   "); break;
+				case 1:		printf("| O "); break;
+				case 2:		printf("| X "); break;
+				case 3:		printf("| ~ "); break;
+			   default: 	printf("|   "); break;
+				}
+			}
+		printf("|");
+		printf("\n");
+		}
+		printf("-----------------------------------------");
+	}
+
